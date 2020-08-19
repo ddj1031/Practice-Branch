9e643a317ce022b6a7b0d92a62fadc4782715c15
Derrick Jackson <derrickdejuanjackson@gmail.com>
2020-08-19 15:21:07 -0400

    Added file-spy maven project as well as another maven project and junit test class for practice

---
+++ JavaJunitPractice/src/main/java/Test1.java
@@ -0,0 +1,21 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		
+		String[] fruit = {"apple", "banana", "cherry"};
+
+		
+		String[][] multiFruit = {
+				{"apple", "banana", "cherry"},
+				{"durian", "eggfruit", "fig"}
+		};
+		
+		System.out.println(fruit[0]);
+		
+		System.out.println(multiFruit[0][0]);
+		System.out.println(multiFruit[1][2]);
+	}
+
+}
