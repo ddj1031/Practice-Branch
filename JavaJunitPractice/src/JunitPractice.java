f82bfd6c80f5459ac0a55c10629e635591ef521c
Derrick Jackson <derrickdejuanjackson@gmail.com>
2020-08-20 08:54:17 -0400

    deleted some files and re made file structure, added new java practice

---
+++ JavaJunitPractice/src/JunitPractice.java
@@ -0,0 +1,21 @@
+
+public class JunitPractice {
+
+	public static void main(String[] args) {
+		
+		String[][] fruit = {
+				{"apple", "banana", "cherry"}
+		};
+		
+		String[][] multiFruit = {
+				{"apple", "banana", "cherry"},
+				{"durian", "eggfruit", "fig"}
+		};
+		
+		System.out.println(fruit[0][0]);
+		System.out.println(multiFruit[0][1]);
+		System.out.println(multiFruit[1][0]);
+
+	}
+
+}
